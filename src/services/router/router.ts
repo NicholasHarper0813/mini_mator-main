55efadd3475a057e433c070ad74ec8f26804f6d6
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 19:30:54 -0800

    Update router.ts

--- src/services/router/router.ts
+++ src/services/router/router.ts
@@ -1,38 +1,47 @@
 import { PageComponent } from '../../components/page.cmp.js';
 
-export interface Route {
+export interface Route 
+{
   name: string;
   path: RegExp;
   buildView: RouteCallback;
 }
 type RouteCallback = (args: string[]) => PageComponent | undefined;
 
-export class Router {
+export class Router 
+{
   configs: Route[] = [];
   onChange = (newPage?: PageComponent) => {};
 
-  constructor(public isDebugMode = true) {
+  constructor(public isDebugMode = true) 
+  {
     this.hashChange = this.hashChange.bind(this);
     window.addEventListener('hashchange', this.hashChange, false);
   }
 
-  addRoute(config: Route) {
+  addRoute(config: Route) 
+  {
     this.configs.push(config);
   }
 
-  getPath() {
+  getPath() 
+  {
     return window.location.hash.substr(1);
   }
 
-  hashChange() {
+  hashChange() 
+  {
     const path = this.getPath();
-    for (let i = 0; i < this.configs.length; i++) {
+    for (let i = 0; i < this.configs.length; i++) 
+    {
       const config = this.configs[i];
       const pathConfig = config.path;
       const exec = pathConfig.exec(path);
       pathConfig.lastIndex = 0;
-      if (exec) {
-        if (this.isDebugMode) {
+      if (exec) 
+      {
+        if (this.isDebugMode) 
+        {
           console.info(`New Route: ${config.name}`, exec.slice(1));
         }
         const newPage = config.buildView(exec.slice(1));
@@ -40,7 +49,8 @@ export class Router {
         return;
       }
     }
-    if (this.isDebugMode) {
+    if (this.isDebugMode) 
+    {
       console.info(`No Route found for "${path}"`);
     }
     this.onChange();

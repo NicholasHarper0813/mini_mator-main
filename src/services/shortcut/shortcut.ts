5984c057fc3961269b5b4592aa0462c953b66644
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:49:40 -0800

    Update shortcut.ts

--- src/services/shortcut/shortcut.ts
+++ src/services/shortcut/shortcut.ts
@@ -5,9 +5,6 @@ export class Shortcut {
   keyupListener: (event: KeyboardEvent) => void;
   keydownListener: (event: KeyboardEvent) => void;
 
-  /**
-   * Init the object by listeneing to the key events
-   */
   constructor() {
     this.keyupListener = this.onKeyUp.bind(this);
     this.keydownListener = this.onKeyDown.bind(this);
@@ -15,18 +12,15 @@ export class Shortcut {
     window.addEventListener('keydown', this.keydownListener);
   }
 
-  /**
-   * Public method to listen keybind
-   * @param string eventName Event to listen to
-   * @param function listener Listener
-   * @return function Executable to remove the listener
-   */
-  on(eventName: string, listener: listener) {
-    if (![...EVENTS, ...MODIFIERS].find(x => x.name===eventName)) {
+  on(eventName: string, listener: listener) 
+  {
+    if (![...EVENTS, ...MODIFIERS].find(x => x.name===eventName)) 
+    {
       throw new Error('Ask to listen for a non existing shortcut');
     }
     let listeners = this.listeners.get(eventName);
-    if (!listeners) {
+    if (!listeners) 
+    {
       listeners = [];
       this.listeners.set(eventName, listeners);
     }
@@ -39,69 +33,65 @@ export class Shortcut {
       }
     };
   }
-
-  /**
-   * Listener for key up
-   * @param event event Event object from window
-   */
-  onKeyUp(event: KeyboardEvent) {
+  
+  onKeyUp(event: KeyboardEvent)
+  {
     this.updateMetaState(event);
-    for (let eventSpecs of MODIFIERS) {
-      if (event.key === eventSpecs.key) {
+    for (let eventSpecs of MODIFIERS) 
+    {
+      if (event.key === eventSpecs.key) 
+      {
         this.dispatch(eventSpecs.name, false);
       }
     }
   }
 
-  /**
-   * Listener for key down
-   * @param event event Event object from window
-   */
-  onKeyDown(event: KeyboardEvent) {
+  onKeyDown(event: KeyboardEvent) 
+  {
     this.updateMetaState(event);
     const key = event.key.toLowerCase();
-    // let areSpecsPassing;
-    for (let eventSpecs of EVENTS) {
-      if (
-        eventSpecs.key === key &&
+    for (let eventSpecs of EVENTS) 
+    {
+      if (eventSpecs.key === key &&
         (eventSpecs.ctrlKey === undefined || eventSpecs.ctrlKey === this.isCtrlMetaOn) &&
-        (eventSpecs.shiftKey === undefined || eventSpecs.shiftKey === event.shiftKey)
-      ) {
+        (eventSpecs.shiftKey === undefined || eventSpecs.shiftKey === event.shiftKey)) 
+      {
         this.dispatch(eventSpecs.name);
         event.preventDefault();
         return;
       } 
     }
-    for (let eventSpecs of MODIFIERS) {
-      if (event.key === eventSpecs.key) {
+    for (let eventSpecs of MODIFIERS) 
+    {
+      if (event.key === eventSpecs.key) 
+      {
         this.dispatch(eventSpecs.name, true);
       }
     }
   }
 
-  updateMetaState(event: KeyboardEvent) {
+  updateMetaState(event: KeyboardEvent) 
+  {
     const isCtrlMetaOn = event.ctrlKey || event.metaKey;
-    if (this.isCtrlMetaOn === isCtrlMetaOn) {
+    if (this.isCtrlMetaOn === isCtrlMetaOn) 
+    {
       return;
     }
 
-    // Set the new state
     this.isCtrlMetaOn = isCtrlMetaOn;
     this.dispatch('ctrl', isCtrlMetaOn);
   }
 
-  dispatch(eventName: string, isOn?: boolean) {
+  dispatch(eventName: string, isOn?: boolean) 
+  {
     let listeners = this.listeners.get(eventName) || [];
     for (let listenerIndex in listeners) {
       listeners[listenerIndex](isOn);
     }
   }
 
-  /**
-   * Public method to remove all events
-   * to make the instance killable
-   */
-  destroy() {
+  destroy() 
+  {
     window.removeEventListener('keyup', this.keyupListener);
     window.removeEventListener('keydown', this.keydownListener);
     this.listeners.clear();
@@ -168,7 +158,8 @@ const MODIFIERS: EventDef[] = [
 ];
 
 type listener = (isOn?: boolean) => void;
-interface EventDef {
+interface EventDef 
+{
   name: string;
   key: string;
   ctrlKey?: boolean;

7dd776e5f48a6702aaa03c5e74003356eaa89616
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:52:40 -0800

    Update base.cmp.ts

--- src/components/base.cmp.ts
+++ src/components/base.cmp.ts
@@ -1,18 +1,9 @@
-/**
- * Decorator for components
- * - Register the element in the custom element registry
- * - Prefetch CSS style for later use
- * @param name Component name for element registry
- * @param cssLink Link to the CSS style sheet
- */
-export function Component(name: string, cssLink?: string) { 
-  return function (constructor: any) { 
+export function Component(name: string, cssLink?: string) 
+{ 
+  return function (constructor: any) 
+  { 
     constructor.prototype.cssLink = cssLink;
-
-    // Register component
     customElements.define(name, constructor);
-
-    // Prefetch Style
     if (cssLink) {
       const link = document.createElement('link');
       link.setAttribute('rel', 'prefetch');
@@ -22,23 +13,15 @@ export function Component(name: string, cssLink?: string) {
   }
 }
 
-/**
- * Root class for components
- */
 @Component('base-cmp')
 export class BaseComponent extends HTMLElement {
-  
   cssLink?: string;
   refs = new Map<string, Element>();
 
   constructor(html: string = '') {
-    // Call parent
     super();
-
-    // Create a shadow root
     this.attachShadow({ mode: 'open' });
 
-    // Create some CSS to apply to the shadow dom
     const {cssLink} = this.constructor.prototype;
     if (cssLink) {
       this.style.visibility = 'hidden';
@@ -55,19 +38,18 @@ export class BaseComponent extends HTMLElement {
       };
       this.shadowRoot?.append(link);
     }
-
-    // Build the node
+    
     const container = document.createElement('div');
-    container.innerHTML = html;
-
-    // Find refs
     const refs = container.querySelectorAll('[data-ref]');
+    container.innerHTML = html;
     refs.forEach((bit) => {
       const bitName = bit.getAttribute('data-ref');
-      if (bitName === null) {
+      if (bitName === null) 
+      {
         return;
       }
-      if (this.refs.get(bitName)) {
+      if (this.refs.get(bitName)) 
+      {
         throw new Error(
           `BaseComponent has been created with duplicated key for '${bitName}'`
         );
@@ -75,7 +57,6 @@ export class BaseComponent extends HTMLElement {
       this.refs.set(bitName, bit);
     });
 
-    // Append child
     this.shadowRoot?.append(...container.children);
   }
 }

6d15eefae1807089aa754f44e5ca8a71477812a6
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-12 06:40:39 -0800

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -4,8 +4,8 @@ import { samples } from './services/samples.js';
 
 export const store: Storage<ProjectItem> = new Storage('minimator-app');
 
-// Add demo samples
-if (store.getNextIndex() === 0) {
+if (store.getNextIndex() === 0) 
+{
   samples.forEach(sample => {
     store.createItem(sample.name, sample.data);
   })

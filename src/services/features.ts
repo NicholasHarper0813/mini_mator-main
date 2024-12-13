afdd693e19549dcaebde47bd6deb7a102cbe4b12
nicholas harper <nicholasharper000@gmail.com>
2024-12-12 23:33:08 -0800

    Update features.ts

--- src/services/features.ts
+++ src/services/features.ts
@@ -1,24 +1,30 @@
-export function share(title: string, url: string, file: File) {
+export function share(title: string, url: string, file: File)
+{
   const basicShare = {
     url,
     title,
   };
+  
   const fullShare = {
     ...basicShare,
     files: [file],
   };
+  
   if (!(navigator as any).canShare) 
   {
     window.alert(`The sharing feature isn't available in your browser`);
   } 
+  
   else if ((navigator as any).canShare(fullShare)) 
   {
     navigator.share(fullShare);
   } 
+  
   else if ((navigator as any).canShare(basicShare)) 
   {
     navigator.share(basicShare);
   } 
+  
   else 
   {
     window.alert(`The sharing feature isn't available in your browser`);
@@ -32,12 +38,12 @@ export function buildPNG(svg: SVGElement): Promise<File>
   canvas.height = parseInt(svg.getAttribute('height') || '0', 10);
 
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
   const blob = new Blob([svg.outerHTML], { type: 'image/svg+xml' });
   const url = window.URL.createObjectURL(blob);
 
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#fff';
+  
   return new Promise((res, rej) => {
     const baseimage = new Image();
     baseimage.style.background = '#fff';
@@ -48,25 +54,26 @@ export function buildPNG(svg: SVGElement): Promise<File>
         res(file);
       });
     }
+    
     baseimage.onerror = rej;
     baseimage.src = url;
   });
 }
 
 let downloadAnchor = document.createElement('a');
-downloadAnchor.style.display = 'none';
 document.body.appendChild(downloadAnchor);
+downloadAnchor.style.display = 'none';
 
 export function downloader(svgContent: BlobPart, fileName: string)
 {
   let blob = new Blob([svgContent], { type: 'octet/stream' }),
     url = window.URL.createObjectURL(blob);
-  downloadAnchor.href = url;
-  downloadAnchor.download = fileName;
-  downloadAnchor.click();
-  window.setTimeout(function () {
-    window.URL.revokeObjectURL(url);
-  }, 10);
+    downloadAnchor.href = url;
+    downloadAnchor.download = fileName;
+    downloadAnchor.click();
+    window.setTimeout(function () {
+      window.URL.revokeObjectURL(url);
+    }, 10);
 }
 
 export function securityCheck() 

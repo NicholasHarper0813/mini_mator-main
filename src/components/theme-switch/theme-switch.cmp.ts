cbe624a8d612217b4da2750035e4ecbc54f0dce2
nicholas harper <nicholasharper000@gmail.com>
2024-12-19 04:18:33 -0800

    Update theme-switch.cmp.ts

--- src/components/theme-switch/theme-switch.cmp.ts
+++ src/components/theme-switch/theme-switch.cmp.ts
@@ -1,13 +1,15 @@
-import { icon } from '../../services/feather.icons.js';
 import { BaseComponent, Component } from '../base.cmp.js';
 import { theme, ThemeMode } from '../../services/theme.js';
+import { icon } from '../../services/feather.icons.js';
 
 @Component('theme-switch-cmp', './src/components/theme-switch/theme-switch.style.css')
-export class ThemeSwitchComponent extends BaseComponent {
+export class ThemeSwitchComponent extends BaseComponent 
+{
   htmlDom = document.querySelector('html');
   button: HTMLButtonElement;
 
-  constructor() {
+  constructor() 
+  {
 
     super(`
       <button data-ref="button" class="action-style">
@@ -18,24 +20,27 @@ export class ThemeSwitchComponent extends BaseComponent {
       </button>
     `);
 
-    // Listen for click on the switch
     this.button = this.refs.get('button') as HTMLButtonElement;
     this.button.addEventListener('click', this.switchListener.bind(this));
-
-    // Set toggle state
     this.setState();
   }
 
-  setState() {
-    if (theme.currentMode === ThemeMode.Dark) {
+  setState() 
+  {
+    if (theme.currentMode === ThemeMode.Dark) 
+    {
       this.button.classList.add('on');
-    } else {
+    } 
+    else 
+    {
       this.button.classList.remove('on');
     }
   }
 
-  switchListener (e: Event) {
-    if (e) {
+  switchListener (e: Event) 
+  {
+    if (e) 
+    {
       e.preventDefault();
       e.stopPropagation();
     }
